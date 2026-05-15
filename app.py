496309bb993ffa99f4afd4a72c81e0919455a797
Manaswini <manaswini2212@gmail.com>
2026-05-15 16:10:05 +0530

    Move assets to root directory for deployment

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def compute_subject(subj):
 # ── routes ────────────────────────────────────────────────────────────────────
 @app.route("/")
 def index():
-    return render_template("api\templates\index.html")
+    return render_template("index.html")
 
 
 @app.route("/extract", methods=["POST"])
@@ -215,7 +215,3 @@ def calculate():
         "newCGPA":       new_cgpa,
         "errors":        errors
     })
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
